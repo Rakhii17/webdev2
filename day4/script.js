cab5f26278aa298f606d48ebfdee93d8901b1312
Rakhi <isainirakhi@gmail.com>
2026-02-02 15:11:22 +0530

    first commit

---
+++ day4/script.js
@@ -0,0 +1,23 @@
+// string method
+// slice
+// let str = "JavaScript is fun!"
+// let sliced = str.slice(0,10);
+// console.log(sliced)
+
+//substring
+// let str = "javascript is amazing"
+// let sub = str.substring(11,17);
+// console.log(sub)
+
+//trim
+// let sentence = "    this is a sentence with whitespaces.    "
+// let trimed = sentence.trim() //this is a sentence with spaces.
+
+// let sentence = "i,am,comma,seperated,sentence";
+// let words = sentence.split(",")
+// console.log(words);
+
+// find out the occurence of r in the given string???(using split method)
+let str = "tu meri me tera me tera tu meri"
+let splitsr = str.split("r");
+console.log(splitsr)
